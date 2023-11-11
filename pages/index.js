d1e508c31d363a4f4e357f485bd3da50d2a424da
ZhuB <85112881+LittleDragoon@users.noreply.github.com>
2023-11-11 16:58:40 +0100

    Setup of the app in Next and Tailwind

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center from-gray-900 to-gray-700 bg-gradient-to-b min-h-screen ">
+      <div className="font-semibold text-4xl py-8 font-bold text-gray-200">
+        Home Page
+      </div>
+    </div>
+  );
+}
